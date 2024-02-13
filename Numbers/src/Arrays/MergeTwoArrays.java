5300af482239b5303cce14e69cdbb21521a9f03c
AdamsArts <adambhai.mipl@gmail.com>
2024-02-13 15:43:42 +0530

    Checking

--- Numbers/src/Arrays/MergeTwoArrays.java
+++ Numbers/src/Arrays/MergeTwoArrays.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 
 public class MergeTwoArrays {
 	public static void main(String[] args) {
+		//Checking
 		int[] first = { 1, 2, 3, 4, 5, 6 };
 		int[] second = { 7, 8, 9, 10 };
 
